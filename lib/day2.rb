e647bf53aa8637ae7bf3eb80f8173e8911a79d09
David Bull <david@uk-dave.com>
2019-12-07 20:12:00 +0000

    Refactor day 2 part 1

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,22 +1,36 @@
 # frozen_string_literal: true
 
 class Day2
+  OPCODES = {
+    1 => :add,
+    2 => :multiply,
+    99 => :terminate,
+  }.freeze
+
+  # rubocop:disable Metrics/MethodLength
   def solve program
+    memory = program.dup
     pc = 0
-    while pc < program.length
-      opcode = program[pc]
-      case opcode
-      when 1
-        program[program[pc + 3]] = program[program[pc + 1]] + program[program[pc + 2]]
-      when 2
-        program[program[pc + 3]] = program[program[pc + 1]] * program[program[pc + 2]]
-      when 99
-        break
-      else
-        raise "Invalid opcode #{opcode}"
-      end
+    while pc < memory.length
+      opcode = memory[pc]
+      operation = OPCODES[opcode]
+      break if operation == :terminate
+      raise "Invalid opcode #{opcode}" unless operation
+
+      send(operation, memory, *memory[pc + 1..pc + 3])
       pc += 4
     end
-    program
+    memory
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  private
+
+  def add memory, arg1, arg2, arg3
+    memory[arg3] = memory[arg1] + memory[arg2]
+  end
+
+  def multiply memory, arg1, arg2, arg3
+    memory[arg3] = memory[arg1] * memory[arg2]
   end
 end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -4,9 +4,17 @@ require "day2"
 
 describe Day2 do
   describe "#solve" do
-    it do
+    it "adds 2 numbers and terminates" do
       expect(subject.solve([1, 5, 6, 3, 99, 10, 20])).to eq [1, 5, 6, 30, 99, 10, 20]
-      expect(subject.solve([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])).to eq [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+    end
+
+    it "multiplies 2 numbers and terminates" do
+      expect(subject.solve([2, 5, 6, 3, 99, 10, 20])).to eq [2, 5, 6, 200, 99, 10, 20]
+    end
+
+    it "runs a more complicated program" do
+      expect(subject.solve([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])).to eq(
+        [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
     end
   end
 end
