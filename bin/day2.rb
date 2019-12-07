d1ec4f652b65a090d0d1822e9b05c392d0c908bf
David Bull <david@uk-dave.com>
2019-12-07 21:46:10 +0000

    Day 2 part 2

--- bin/day2.rb
+++ bin/day2.rb
@@ -6,7 +6,20 @@ require "day2"
 
 day2 = Day2.new
 program = File.read("fixtures/day2.txt").split(",").map(&:to_i)
+
 program[1] = 12
 program[2] = 2
 result = day2.solve(program)
 puts result[0]
+
+(0..100).each do |noun|
+  program[1] = noun
+  (0..100).each do |verb|
+    program[2] = verb
+    result = day2.solve(program)
+    if result[0] == 19_690_720
+      puts 100 * noun + verb
+      exit
+    end
+  end
+end
